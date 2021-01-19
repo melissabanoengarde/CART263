d45859759cdd32ac373f391ed8d27e2cdec8aaba
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2021-01-19 18:29:07 -0500

    added new W2 folder

---
+++ experiments/W2 - review/js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/*****************
+
+W2 - Review
+Melissa Banoen-Garde
+
+Going over constants, object parameters,
+and first-class function
+
+******************/
+
+// preload()
+// Description of preload
+function preload() {
+
+}
+
+
+// setup()
+// Description of setup
+function setup() {
+
+}
+
+
+// draw()
+// Description of draw()
+function draw() {
+
+}
