c0adb951bcd37a7639146a1d203076ee84a03ee0
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2021-01-21 22:00:15 -0500

    followed through video 2.1: Constants
    conclusion: constants are variables that cannot be changed and are written in all caps and underscores.
    
    viewed examples (video and notes) how to use in practice.

--- experiments/W2 - review/js/script.js
+++ experiments/W2 - review/js/script.js
@@ -10,22 +10,42 @@ and first-class function
 
 ******************/
 
-// preload()
-// Description of preload
-function preload() {
+// Constants defining our grid
+const TILE_SIZE = 50;
+const COLUMNS = 10;
+const ROWS = 10;
 
-}
-
-
-// setup()
-// Description of setup
+// Creates the canvas
 function setup() {
-
+  // Define the canvas size in terms of the grid constants
+  createCanvas(TILE_SIZE * COLUMNS, TILE_SIZE * ROWS);
 }
 
-
-// draw()
-// Description of draw()
+// Draw a sparkling grid
 function draw() {
+  background(0);
 
+  // Loop through each row
+  for (let row = 0; row < ROWS; row++) {
+    // Loop through each column
+    for (let col = 0; col < COLUMNS; col++) {
+      // Draw a tile at the current row and column
+      drawTile(row, col);
+    }
+  }
+}
+
+// Draw a tile at the specified row and column
+function drawTile(row, col) {
+  // Calculate the x and y of the tile on the canvas
+  // by multiplying by the tile size
+  let x = row * TILE_SIZE;
+  let y = col * TILE_SIZE;
+  // Draw a random shaded tile sized square at that location
+  push();
+  noStroke();
+  let grey = random(50, 200);
+  fill(grey);
+  rect(x, y, TILE_SIZE, TILE_SIZE);
+  pop();
 }
