aa178bb96f6a08cc6240e87fc2cd74abac5e7191
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2021-01-22 11:19:53 -0500

    followed through and completed "2.2: object parameters"
    learned about object literals and object destructuring

--- experiments/W2 - review/js/script.js
+++ experiments/W2 - review/js/script.js
@@ -10,42 +10,34 @@ and first-class function
 
 ******************/
 
-// Constants defining our grid
-const TILE_SIZE = 50;
-const COLUMNS = 10;
-const ROWS = 10;
-
-// Creates the canvas
 function setup() {
-  // Define the canvas size in terms of the grid constants
-  createCanvas(TILE_SIZE * COLUMNS, TILE_SIZE * ROWS);
+  createCanvas(500, 500);
 }
 
-// Draw a sparkling grid
 function draw() {
   background(0);
 
-  // Loop through each row
-  for (let row = 0; row < ROWS; row++) {
-    // Loop through each column
-    for (let col = 0; col < COLUMNS; col++) {
-      // Draw a tile at the current row and column
-      drawTile(row, col);
-    }
-  }
+  let config = {
+    x: 250,
+    y:250,
+    width:200,
+    height:200,
+    fillColour: {
+      r:255,
+      g:255,
+      b:0
+    },
+    mode: CENTER
+  };
+
+  drawFancyRect(config);
 }
 
-// Draw a tile at the specified row and column
-function drawTile(row, col) {
-  // Calculate the x and y of the tile on the canvas
-  // by multiplying by the tile size
-  let x = row * TILE_SIZE;
-  let y = col * TILE_SIZE;
-  // Draw a random shaded tile sized square at that location
+// destructring..
+function drawFancyRect({ x, y, width, height, fillColour, mode}) {
   push();
-  noStroke();
-  let grey = random(50, 200);
-  fill(grey);
-  rect(x, y, TILE_SIZE, TILE_SIZE);
+  fill(fillColour.r, fillColour.g, fillColour.b);
+  rectMode(mode);
+  rect(x, y, width, height);
   pop();
 }
