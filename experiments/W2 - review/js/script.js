704ab7ba5608509de9eed7855da941b8eb66f11d
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2021-01-22 12:00:54 -0500

    followed through and completed video "2.3: First-class functions"
    learned about first-class functions, functions as arguments and anonymous functions. will check out TMI another time/later.

--- experiments/W2 - review/js/script.js
+++ experiments/W2 - review/js/script.js
@@ -10,34 +10,7 @@ and first-class function
 
 ******************/
 
-function setup() {
-  createCanvas(500, 500);
-}
 
-function draw() {
-  background(0);
-
-  let config = {
-    x: 250,
-    y:250,
-    width:200,
-    height:200,
-    fillColour: {
-      r:255,
-      g:255,
-      b:0
-    },
-    mode: CENTER
-  };
-
-  drawFancyRect(config);
-}
-
-// destructring..
-function drawFancyRect({ x, y, width, height, fillColour, mode}) {
-  push();
-  fill(fillColour.r, fillColour.g, fillColour.b);
-  rectMode(mode);
-  rect(x, y, width, height);
-  pop();
-}
+setTimeout(function () {
+  alert(`Hello!`);
+}, 5000);
