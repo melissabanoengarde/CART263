e3a15683a592174325ed55f2273c44ab7f7de582
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2021-01-22 13:55:25 -0500

    E1: Starting code

---
+++ exercises/E1-wheres-sausage-dog-new-game-plus/js/SausageDog.js
@@ -0,0 +1,32 @@
+// Sausage Dog
+// subclass of super class Animal.js
+
+class SausageDog extends Animal{
+
+  constructor(x,y,image) {
+
+    // call superclass constructor
+    super(x,y,image);
+
+    // tracking finding
+    this.found = false;
+    this.rotationSpeed = 0.25;
+  }
+
+  // when the sausge dog is found, it spins
+  update() {
+    super.update();
+
+    if (this.found) {
+      // we can use this.angle because it is inherited from the superclass
+      this.angle += this.rotationSpeed;
+    }
+  }
+
+  // establishes the constraints of the sausage dog's image's clickability (? can't formulate a sentence)
+  // if the mouse is clicked inside the sausage dog image, the found property is set to "true"
+  mousePressed() {
+    super.overlap();
+  }
+
+}
