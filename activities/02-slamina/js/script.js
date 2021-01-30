9f479f42c11c4987eb782a1ae0d7879592b22712
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2021-01-30 14:41:59 -0500

    A2: added the responsiveVoice and annyang library

--- activities/02-slamina/js/script.js
+++ activities/02-slamina/js/script.js
@@ -2,11 +2,13 @@
 
 /*****************
 
-Title of Project
-Author Name
+Activity 02: Slamina
+Melissa Banoen-Garde
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
++ obtain a list of animal names for the user to guess
++ figure out how to make ResponsiveVoice say an animal's name backwards
++ need to have annyang listen to a user command representing a guess
++ need to display whether they got it right
 
 ******************/
 
