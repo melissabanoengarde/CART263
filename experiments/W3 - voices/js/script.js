5456b6a2a944d153ac505179938308b5bfd338f0
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2021-01-29 22:15:42 -0500

    having fun with responsiveVoice

--- experiments/W3 - voices/js/script.js
+++ experiments/W3 - voices/js/script.js
@@ -19,12 +19,20 @@ function preload() {
 // setup()
 // Description of setup
 function setup() {
-
+  createCanvas(500,500);
 }
 
 
 // draw()
 // Description of draw()
 function draw() {
+  background(255,0,0);
+}
 
+function mousePressed() {
+  responsiveVoice.speak("As I walk through the valley and the shadow of death, I aaaaaam... yourrrrr... father", "UK English Male", {
+    pitch: .5,
+    rate: 0.5,
+    volume: 1
+  });
 }
