ddb7e9184d0df901a1065ab337ee1f5d70540ce5
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2021-01-30 14:34:57 -0500

    A1: starting slamina activity

--- experiments/W3 - voices/js/script.js
+++ experiments/W3 - voices/js/script.js
@@ -9,30 +9,49 @@ Experimenting with voices!
 
 ******************/
 
-// preload()
-// Description of preload
-function preload() {
+// Default name
+let userName = `stranger`
 
-}
-
-
-// setup()
-// Description of setup
 function setup() {
-  createCanvas(500,500);
+  createCanvas(windowWidth, windowHeight);
+
+  if (annyang) {
+    let command = {
+      // A command that listens for "my name is..." and the captures
+      // whatever they say after that and sends it as an argument to setName()
+      // Remember that anything after a splat (*) is captured and passes it to a function
+      'My name is *name': setName
+    }
+    annyang.addCommands(command);
+    annyang.start();
+  }
 }
 
+// Sets the current username to whatever argument is passed to it by annyang!
+// Now how what the user said will be passed into the parameter called name
+function setName(name) {
+  userName = name;
+}
 
-// draw()
-// Description of draw()
 function draw() {
-  background(255,0,0);
+  background(0);
+
+  // Greet the user
+  push();
+  fill(255, 255, 0);
+  textSize(32);
+  rectMode(CENTER);
+  text(`Hi there, ${userName}!`, 100, 100);
+  pop();
 }
 
-function mousePressed() {
-  responsiveVoice.speak("As I walk through the valley and the shadow of death, I aaaaaam... yourrrrr... father", "UK English Male", {
-    pitch: .5,
-    rate: 0.5,
-    volume: 1
-  });
-}
+
+
+// RESPONSIVEVOICE
+// function mousePressed() {
+//   responsiveVoice.speak("As I walk through the valley and the shadow of death, I aaaaaam... yourrrrr... father", "UK English Male", {
+//     pitch: .5,
+//     rate: 0.5,
+//     volume: 1
+//   });
+// }
