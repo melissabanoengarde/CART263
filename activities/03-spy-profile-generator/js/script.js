d4db50639d507d5fd6c0bc5f864762f3df6594e8
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2021-02-08 21:42:33 -0500

    A3: prompts user to provide name and returns it while storing input as name data under spyProfile object

--- activities/03-spy-profile-generator/js/script.js
+++ activities/03-spy-profile-generator/js/script.js
@@ -2,32 +2,46 @@
 
 /*****************
 
-Activity 04: Spy Profile Generator
+Activity 03: Spy Profile Generator
 Melissa Banoen-Garde
 
 + get user's name and display a default profile
 + generate a profile instead using JSON data
 + save and load the generated profile
-+ add a password check 
++ add a password check
 
 ******************/
 
-// preload()
-// Description of preload
-function preload() {
-
-}
+// global variable
+let spyProfile = {
+  name: `REDACTED`,
+  alias: `REDACTED`,
+  secretWeapon: `REDACTED`,
+  password: `REDACTED`
+};
 
 
 // setup()
 // Description of setup
 function setup() {
+  createCanvas(380, 550);
 
+  // prompts user to provide name
+  spyProfile.name = prompt(`Please provide your name.`);
 }
 
 
 // draw()
 // Description of draw()
 function draw() {
+  background(8, 25, 74);
 
+  // displays name input
+  push();
+  textFont('Courier');
+  textSize(20);
+  textAlign(CENTER, CENTER);
+  fill(230, 209, 90);
+  text(`Greetings, ${spyProfile.name}.`, width/2, height/4);
+  pop();
 }
