4992ead3436f350f4ecb6456131e2e72f41b2925
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2021-02-13 22:26:06 -0500

    E4: Starting code, added multiple bubbles

---
+++ exercises/E4-bubble-popper-plus/js/Bubble.js
@@ -0,0 +1,39 @@
+class Bubble {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.vx = 0;
+    this.vy = -2; // for element to rise
+    this.active = true;
+  }
+
+  // bubbles' movement: bubbles rise from the bottom of the canvas
+  motion() {
+    this.x += this.vx;
+    this.y += this.vy;
+
+    if (this.y < 0) {
+      this.x = random(width);
+      this.y = height;
+    }
+  }
+
+  // draws bubbles
+  display() {
+    push();
+    fill(174, 226, 242);
+    noStroke();
+    ellipse(this.x, this.y, this.size);
+    pop();
+  }
+
+  // popped() {
+  //   if (this.active) {
+  //     fill(255,100,77);
+  //     noStroke();
+  //     ellipse(this.x, this.y, this.size);
+  //   }
+  // }
+
+}
--- exercises/E4-bubble-popper-plus/js/script.js
+++ exercises/E4-bubble-popper-plus/js/script.js
@@ -2,13 +2,29 @@
 
 /*****************
 
-Title of Project
-Author Name
+E4: Bubbble Popper +
+Melissa Banoen-Garde
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+pop bubbbles by matching pin and bubble colour
+- adding multiple bubbles
+
 
 ******************/
+
+// global video variable
+let video;
+
+// global handpose variable
+let handpose = undefined;
+
+// global predictions variable
+let predictions = [];
+
+// bubbles array
+let bubbles = [];
+let totalBubbles = 10;
+// variable to store our bubble
+let bubble;
 
 // preload()
 // Description of preload
@@ -20,12 +36,100 @@ function preload() {
 // setup()
 // Description of setup
 function setup() {
+  createCanvas(640, 480);
 
+  // WEBCAM
+  // accesses the webcam
+  video = createCapture(VIDEO);
+  // hides video element so it doesn't display on the page
+  video.hide();
+
+  // HANDPOSE
+  // handpose = ml5.handpose(?video, ?options, ?callback);
+  handpose = ml5.handpose(video, {
+    flipHorizontal: true
+  }, function() {
+    console.log(`Model loaded.`)
+  });
+  // listens for predictions
+  // keeps predictions array up to date with the latest result
+  handpose.on(`predict`, function(results) {
+    console.log(results);
+    predictions = results;
+  });
+
+  // BUBBLE
+  for (let i = 0; i < totalBubbles; i++) {
+    // defining parameters of the bubbles
+    let x = random(width);
+    let y = random(height, 500);
+    let size = random(10, 50);
+    // creating a new object to call the Bubble.js class
+    bubble = new Bubble(x, y, size);
+    // pushing new object in the "bubbles" array
+    bubbles.push(bubble);
+  }
 }
 
 
 // draw()
 // Description of draw()
 function draw() {
-
+  background(45, 161, 62); // green
+  pins();
+  bubblePop();
 }
+
+function pins() {
+  // checks if predictions aaray has anything in it (length)
+  if (predictions.length > 0) {
+    let hand = predictions[0]; // handpose only predicts a single hand
+    let index = hand.annotations.indexFinger;
+    let tip = index[3];
+    let base = index[0];
+    let tipX = tip[0];
+    let tipY = tip[1];
+    let baseX = base[0];
+    let baseY = base[1];
+
+    // drawing the pin
+    // pin body
+    push();
+    stroke(255);
+    line(baseX, baseY, tipX, tipY);
+    pop();
+
+    // pin head
+    push();
+    noStroke();
+    fill(255, 0, 0);
+    ellipse(baseX, baseY, 20);
+    pop();
+
+    // check bubble popping
+    // calculates the distance between the bubble and the pin
+    // let d = dist(tipX, tipY, bubble.x, bubble.y);
+    // // if the distance is less than half the size of the bubble
+    // if (d < bubble.size / 2) {
+    //   // the bubble resets (starts from the bottom again )
+    //   bubble.x = random(width);
+    //   bubble.y = height;
+    // }
+  }
+}
+
+function bubblePop() {
+  // calling the Bubble class object's methods
+  for (let i = 0; i < bubbles.length; i++) {
+    let bubble = bubbles[i];
+    bubble.motion();
+    bubble.display();
+  }
+}
+
+// bubble's display
+// push();
+// fill(174, 226, 242);
+// noStroke();
+// ellipse(bubble.x, bubble.y, bubble.size);
+// pop();
