54727cf4edbf2b16997f23447c84e181df1f81c3
Melissa Banoen-Garde <m.banoengarde@gmail.com>
2021-02-17 22:56:30 -0500

    P1: Starting code

--- projects/P1/js/script.js
+++ projects/P1/js/script.js
@@ -1,13 +1,8 @@
 "use strict";
 
 /*****************
-
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
-
+Project 1
+Melissa Banoen-Garde
 ******************/
 
 // preload()
